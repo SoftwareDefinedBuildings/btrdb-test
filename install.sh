47ed593fde8cb7c1c0d693b27342d93766571a02
Sam Kumar <samkumar99@gmail.com>
2016-03-22 17:28:05 -0700

    Updated install script

--- install.sh
+++ install.sh
@@ -7,39 +7,43 @@ then
     exit 1
 fi
 
+mkdir -p installed
+
 # Apt-Get Install Dependencies
-apt-get -y install gcc git librados-dev
+apt-get -y install gcc git librados-dev mongodb
 sync
 
 # Install Go 1.6
-if [ $(which go) ]
+if [ $(ls installed | grep go-installed) ]
 then
     echo "Go is already installed"
 else
     wget https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz
     tar -C /usr/local -xzf go1.6.linux-amd64.tar.gz
     export PATH=$PATH:/usr/local/go/bin
-    sync
     echo "export PATH=\$PATH:/usr/local/go/bin" >> $HOME/.profile
+    sync
+    date > installed/go-installed
     sync
 fi
 
 # Install BTrDB
-if [ $(which btrdbd) ]
+if [ $(ls installed | grep btrdb-installed) ]
 then
     echo "BTrDB is already installed"
 else
-    mkdir btrdb
+    mkdir -p btrdb
     export GOPATH=$(pwd)/btrdb
     go get github.com/SoftwareDefinedBuildings/btrdb/btrdbd
     export PATH=$PATH:$GOPATH/bin
-    sync
     echo "export PATH=\$PATH:$GOPATH/bin" >> $HOME/.profile
+    sync
+    date > installed/btrdb-installed
     sync
 fi
 
 # Install the Load Generator
-if [ $(which quasarLoadGenerator) ]
+if [ $(ls installed | grep qlg-installed) ]
 then
     echo "QuasarLoadGenerator is already installed"
 else
@@ -48,11 +52,12 @@ else
     export GOPATH=$(pwd)/qlg
     go get github.com/SoftwareDefinedBuildings/quasarLoadGenerator
     export PATH=$PATH:$GOPATH/bin
-    sync
     echo "export PATH=\$PATH:$GOPATH/bin" >> $HOME/.profile
+    sync
+    date > installed/qlg-installed
     sync
 fi
 
 # Mark Completion
-date > INSTALLED
+date > installed/all-installed
 sync
